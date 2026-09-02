bc351a03894aced4212ec411675413c18eace19b
Basil-AS <71924962+Basil-AS@users.noreply.github.com>
2026-09-02 13:07:07 +0300

    chore: load Play runtime v5

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import "./runtime-fixes.js?v=20260902-4";
+import "./runtime-fixes.js?v=20260902-5";
 import { $,dom,state,PACKAGE_RE,PACKAGE_PREFIX_RE,PACKAGE_LIKE_RE,transport } from "./common.js?v=20260902-9";
 import { setStatus,emptyState } from "./render.js?v=20260902-10";
 import { searchApps,updateSuggestions,selectPackage,resolveSelected,makeApks,resetApp } from "./actions.js?v=20260902-9";
