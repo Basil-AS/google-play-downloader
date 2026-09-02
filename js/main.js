b1fee3eb34b63521f73dadc2b0a1281122d17d87
Basil-AS <71924962+Basil-AS@users.noreply.github.com>
2026-09-02 11:47:45 +0300

    chore: bust render module cache

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import "./runtime-fixes.js?v=20260902-1";
 import { $,dom,state,PACKAGE_RE,PACKAGE_PREFIX_RE,PACKAGE_LIKE_RE,transport } from "./common.js?v=20260902-8";
-import { setStatus,emptyState } from "./render.js?v=20260902-8";
+import { setStatus,emptyState } from "./render.js?v=20260902-9";
 import { searchApps,updateSuggestions,selectPackage,resolveSelected,makeApks,resetApp } from "./actions.js?v=20260902-8";
 
 const SEARCH_CACHE_MARKER = "gpd:search:transport-version-v2";
